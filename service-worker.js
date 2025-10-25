f5743729389397be376909c9bb85bde3c335f1e7
kcsrtreasures <kcsrdev@gmail.com>
2025-10-25 20:29:44 +0800

    many changes

--- service-worker.js
+++ service-worker.js
@@ -1,36 +1,42 @@
-const CACHE_NAME = "baking-timer-v3";
-const ASSETS = [
-  "./",
-  "./timer.html",
-  "./breadroll.png",
-  "./logo4.png",
-  "./manifest.json",
-  "./alarm digital.mp3",
-  "./huntrix_golden.mp3",
-  "./Soda Pop.mp3",
-  "./timer1.mp3",
-  "./timer2.mp3",
-  "./timer3.mp3",
-
+const CACHE_NAME = 'baking-timer-v3';
+const FILES_TO_CACHE = [
+  '/',
+  '/timer.html',
+  '/styles.css',
+  '/scripts.js',
+  '/breadroll.png',
+  '/logo4.png',
+  '/sounds/alarm digital.mp3',
+  '/sounds/huntrix_golden.mp3',
+  '/sounds/Soda Pop.mp3',
+  '/sounds/timer1.mp3',
+  '/sounds/timer2.mp3',
+  '/sounds/timer3.mp3'
 ];
 
 // Activate immediately after install (optional but recommended)
 self.skipWaiting();
 
-self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
-    )
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
   );
 });
 
-
-self.addEventListener("install", (e) => {
-  console.log("📦 Service Worker installing...");
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
 });
